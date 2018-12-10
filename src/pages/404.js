8330acb1495c8a8b5a9e41e4c18bca9deb378e21
Aleksander Hansson <>
2018-12-10 11:37:00 -0800

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,53 @@
 import React from 'react'
 import Layout from '../components/layout'
 
-const NotFoundPage = () => (
+import {graphql} from 'gatsby'
+import styled, {css} from 'styled-components'
+import Hero from '../components/Hero'
+
+import { media } from '../utils/style'
+
+import HeroText from '../components/heroText'
+import Social from '../components/social'
+
+const StyledHero = styled(Hero)`
+  margin-top:-62px;
+  &>div:nth-child(2) {
+    width:80%;
+    margin:0 auto;
+  }
+`
+
+const NotFoundPage = ({data}) => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <StyledHero fluid={data.hero.edges[0].node.fluid}>
+      <HeroText text="404 Not Found"/>
+      <Social edges={data.allSocialJson.edges}/>
+    </StyledHero>
   </Layout>
 )
 
 export default NotFoundPage
+
+export const pageQuery = graphql`
+  query notFoundQuery {
+    hero: allImageSharp(filter:{original:{src:{regex:"/golden-gate/"}}}) {
+      edges {
+        node {
+          id
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+    allSocialJson {
+      edges {
+        node {
+          url
+          type
+        }
+      }
+    }
+  }
+`
