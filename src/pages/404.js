2d1b81a616eaf0583df5cb5bed6b00faf3c621f0
Aleksander Hansson <hello@ahansson.com>
2018-12-17 12:37:16 -0800

    remove tracedsvg for hero image

--- src/pages/404.js
+++ src/pages/404.js
@@ -28,7 +28,7 @@ export const pageQuery = graphql`
         node {
           id
           fluid(quality: 100) {
-            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            ...GatsbyImageSharpFluid_withWebp
           }
         }
       }
