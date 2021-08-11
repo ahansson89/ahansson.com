f0c0716ffd377e3aefd502c96d84afcfde2cea99
Luca Palonca <luca.palonca@sailogy.com>
2021-08-11 11:38:26 +0200

    refactoring

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'gatsby-image';
 import styled from 'styled-components';
 import media from '../utils/style';
 
@@ -22,24 +21,6 @@ const Overlay = styled.div`
   justify-content: center;
   align-items: center;
   text-align: center;
-`;
-
-const BgImage = styled(Image)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: -1;
-  height: ${(props) => props.height || '100vh'};
-
-  // Adjust image positioning (if image covers area with defined height) and add font-family for polyfill
-  & > img {
-    object-fit: ${(props) => props.fit || 'cover'} !important;
-    object-position: ${(props) => props.position || '50% 50%'} !important;
-    font-family: 'object-fit: ${(props) => props.fit || 'cover'} !important; object-position: ${(
-  props,
-) => props.position || '50% 50%'} !important;'
-  }
 `;
 
 const Video = styled.video`
@@ -85,7 +66,6 @@ function Hero(props) {
   const { children, videos } = props;
   return (
     <Container>
-      {/* <BgImage fluid={fluid} /> */}
       <BgVideo videos={videos} />
       <Overlay>{children}</Overlay>
     </Container>
